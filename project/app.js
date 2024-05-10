1b3796dd5ff298aa8eaf653a70beffe7f91b8a59
Hanzalashaik <shaikhhanzala27@gmail.com>
2024-05-10 15:47:13 +0530

    literals,union,unknown,nerver types.

--- project/app.js
+++ project/app.js
@@ -1,3 +1,4 @@
+"use strict";
 function add(num1, num2, isAdd, phrase) {
     if (isAdd) {
         console.log(phrase, num1 + num2);
@@ -6,32 +7,32 @@ function add(num1, num2, isAdd, phrase) {
         return num1 + num2;
     }
 }
-var resultPhrase = "the addition of two number is :";
+const resultPhrase = "the addition of two number is :";
 add(2, 23, true, resultPhrase);
 //objects
-var obj = {
+const obj = {
     name: "hanzala",
-    age: 44
+    age: 44,
 };
 console.log(obj.age);
 //tuples
-var obj1 = {
+const obj1 = {
     name: "hanzala",
     age: 44,
-    hobbies: ['Sport', 'Cooking'],
-    role: [2, 'author']
+    hobbies: ["Sport", "Cooking"],
+    role: [2, "author"],
 };
-obj1.role.push('admin');
+// obj1.role.push("admin");
 obj1.role[2] = 3;
 console.log(obj1);
 //Enum
+//enum is a set of constants
 var Role;
 (function (Role) {
     Role[Role["ADMIN"] = 0] = "ADMIN";
     Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
     Role[Role["AUTHOR"] = 2] = "AUTHOR";
 })(Role || (Role = {}));
-;
 var obj2 = {
     name: "hanzala",
     age: 44,
@@ -41,3 +42,46 @@ var obj2 = {
 if (obj2.role === Role.ADMIN) {
     console.log("is admin");
 }
+//union types
+function combine(input1, input2) {
+    let result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+const combineAges1 = combine(30, 90);
+console.log(combineAges1);
+const combineNames1 = combine("hanzala", " shaikh");
+console.log(combineNames1);
+function combinable(input1, input2) {
+    let result;
+    if (typeof input1 === "number" && typeof input2 === "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+const combineAges = combine(30, 90);
+console.log(combineAges);
+const combineNames = combine("hanzala", " shaikh");
+console.log(combineNames);
+//unknown type
+let userInput1;
+let userName;
+// userInput1 = 5;
+userInput1 = "Max";
+if (typeof userInput1 === "string") {
+    userName = userInput1;
+    console.log(userName);
+}
+//never type
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError("An error occured!", 502);
--- project/app.ts
+++ project/app.ts
@@ -24,13 +24,12 @@ const obj1 = {
   hobbies: ["Sport", "Cooking"],
   role: [2, "author"],
 };
-obj1.role.push("admin");
+// obj1.role.push("admin");
 obj1.role[2] = 3;
 
 console.log(obj1);
 
 //Enum
-
 //enum is a set of constants
 enum Role {
   ADMIN,
@@ -47,3 +46,58 @@ var obj2 = {
 if (obj2.role === Role.ADMIN) {
   console.log("is admin");
 }
+
+//union types
+
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+const combineAges1 = combine(30, 90);
+console.log(combineAges1);
+
+const combineNames1 = combine("hanzala", " shaikh");
+console.log(combineNames1);
+
+// type alias
+
+type Combinable = number | string;
+
+function combinable(input1: Combinable, input2: Combinable) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+const combineAges = combine(30, 90);
+console.log(combineAges);
+
+const combineNames = combine("hanzala", " shaikh");
+console.log(combineNames);
+
+//unknown type
+let userInput1: unknown;
+let userName: string;
+
+// userInput1 = 5;
+userInput1 = "Max";
+if (typeof userInput1 === "string") {
+  userName = userInput1;
+  console.log(userName);
+}
+
+//never type
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError("An error occured!", 502);
